8004607bb9e5972244eb0a02a9a03cba4dfe975b
Fokusnik909 <Fokusnik90999@gmail.com>
2023-11-17 20:02:02 +0500

    added connections

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    var counter = 1111 {
+        didSet {
+            countLabel.text = "\(counter)"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 26)
+        let image = UIImage(systemName: "arrow.circlepath",withConfiguration: config)
+        resetButton.setImage(image, for: .normal)
+        
+        let cornerRadiusButton = plusButton.frame.width / 2
+        
+        plusButton.layer.cornerRadius = cornerRadiusButton
+        minusButton.layer.cornerRadius = cornerRadiusButton
+    
     }
 
+    @IBAction func plusButtonPressed(_ sender: Any) {
+        counter += 111
+    }
+    
+    @IBAction func minusButtonPressed(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+        }
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        counter = 0
+    }
+    
 
 }
 
